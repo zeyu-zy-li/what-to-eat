823eacbfa104aee23b144fb7c1b8f20949e09dda
zeyu-zy-li <zeyu.zy.li@gmail.com>
2022-01-15 18:49:04 -0500

    remove the exercises

--- test_pytest.py
+++
@@ -1,8 +0,0 @@
-from stock1 import *
-# tests for function addRecipe
-def test_addRecipe_newDish_newFood():
-    con = sqlite3.connect('test3.db')
-    with con:
-        cur = con.cursor()
-        
-        
